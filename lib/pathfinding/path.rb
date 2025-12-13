ed105eb574d84f1253f31f6fdf86e5e7eb9283d8
Cyberarm <matthewlikesrobots@gmail.com>
2025-12-13 16:02:44 -0600

    Stub pathfinding stuff

---
+++ lib/pathfinding/path.rb
@@ -0,0 +1,7 @@
+module IMICTDS
+  module Pathfinding
+    # Path for agent to follow
+    class Path
+    end
+  end
+end
---
+++ lib/pathfinding/pathfinder.rb
@@ -0,0 +1,7 @@
+module IMICTDS
+  module Pathfinding
+    # Find path between to points on the map, if possible.
+    class Pathfinder
+    end
+  end
+end
