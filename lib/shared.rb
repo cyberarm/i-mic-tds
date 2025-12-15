62c52b9edb2f2f22c478a5b4530037a6c236a1ee
Cyberarm <matthewlikesrobots@gmail.com>
2025-12-14 18:04:37 -0600

    Added some helper functions

--- lib/shared.rb
+++ lib/shared.rb
@@ -40,6 +40,7 @@ require_relative "networking/server"
 require_relative "networking/connection"
 require_relative "networking/packet_handler"
 require_relative "networking/packet_handlers/input"
+require_relative "networking/packet_handlers/snapshot"
 
 require_relative "game"
 require_relative "map"
@@ -50,3 +51,36 @@ require_relative "game_modes/bomb_detonation"
 require_relative "game_modes/capture_the_flag"
 require_relative "game_modes/team_deathmatch"
 require_relative "game_modes/king_of_the_hill"
+
+module IMICTDS
+  # Assert that all of a classes children use a unique TYPE (id)
+  def self.assert_subclassed_type_unique(klass)
+    subclasses = klass.subclasses
+    subclasses.each do |klass|
+      subclasses.each do |compared|
+        next if klass == compared
+
+        raise "#{klass} and #{compared} have the same type! (#{klass::TYPE})" if klass::TYPE == compared::TYPE
+      end
+    end
+  end
+
+  def self.format_byte_size(byte_count)
+    case byte_count
+    when 0..1023 # Bytes
+      "#{byte_count} B"
+    when 1024..1_048_575 # KiloBytes
+      "#{format_size_number(byte_count / 1024.0)} KB"
+    when 1_048_576..1_073_741_999 # MegaBytes
+      "#{format_size_number(byte_count / 1_048_576.0)} MB"
+    else # GigaBytes
+      "#{format_size_number(byte_count / 1_073_742_000.0)} GB"
+    end
+  end
+
+  def self.format_size_number(i)
+    format("%0.2f", i)
+  end
+end
+
+IMICTDS.assert_subclassed_type_unique(IMICTDS::Networking::PacketHandler)
